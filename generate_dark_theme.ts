0c8126f932fedf547522976c58ecb1c37bd481bb
cactoes <maartenhvanrooij@gmail.com>
2024-09-16 21:44:08 +0200

    init

--- generate_dark_theme.ts
+++ generate_dark_theme.ts
@@ -1,8 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const theme = JSON.parse(fs.readFileSync(path.join(__dirname, "themes", "cybercore-color-theme.json")).toString());
-
 const color_scale_factor = 0.85;
 
 /**
@@ -33,31 +31,38 @@ function invert_color(hex_code: string): string {
     return `${inverted_string}${inverted_string}${inverted_string}`;
 }
 
-for (const key of Object.keys(theme.colors)) {
-    let vv = theme.colors[key] as string;
-    const hex_code = vv.slice(1, 7);
-    const split_result = hex_code.split(hex_code.slice(0, 2)).join("");
+function generate_dark_theme_for(in_file: string, out_file: string): void {
+    const theme = JSON.parse(fs.readFileSync(path.join(__dirname, "themes", in_file)).toString());
 
-    // single color gray scale
-    if (split_result == "") {
-        theme.colors[key] = vv.replace(hex_code, invert_color(hex_code));
-        continue;
-    } else {
-        theme.colors[key] = vv.replace(hex_code, darken_hex_color(hex_code, color_scale_factor));
-        continue;
+    for (const key of Object.keys(theme.colors)) {
+        let vv = theme.colors[key] as string;
+        const hex_code = vv.slice(1, 7);
+        const split_result = hex_code.split(hex_code.slice(0, 2)).join("");
+    
+        // single color gray scale
+        if (split_result == "") {
+            theme.colors[key] = vv.replace(hex_code, invert_color(hex_code));
+            continue;
+        } else {
+            theme.colors[key] = vv.replace(hex_code, darken_hex_color(hex_code, color_scale_factor));
+            continue;
+        }
     }
+    
+    for (const object of theme.tokenColors) {
+        const hex_code = object.settings.foreground.slice(1, 7);
+        const split_result = hex_code.split(hex_code.slice(0, 2)).join("");
+    
+        // single color gray scale
+        if (split_result == "") {
+            object.settings.foreground = object.settings.foreground.replace(hex_code, invert_color(hex_code));
+        } else {
+            object.settings.foreground = object.settings.foreground.replace(hex_code, darken_hex_color(hex_code, color_scale_factor));
+        }
+    }
+    
+    fs.writeFileSync(path.join(__dirname, "themes", out_file), JSON.stringify(theme, null, 4));
 }
 
-for (const object of theme.tokenColors) {
-    const hex_code = object.settings.foreground.slice(1, 7);
-    const split_result = hex_code.split(hex_code.slice(0, 2)).join("");
-
-    // single color gray scale
-    if (split_result == "") {
-        object.settings.foreground = object.settings.foreground.replace(hex_code, invert_color(hex_code));
-    } else {
-        object.settings.foreground = object.settings.foreground.replace(hex_code, darken_hex_color(hex_code, color_scale_factor));
-    }
-}
-
-fs.writeFileSync(path.join(__dirname, "themes", "cybercore-dark-color-theme.json"), JSON.stringify(theme, null, 4));
+generate_dark_theme_for("cybercore-color-theme.json", "cybercore-dark-color-theme.json");
+generate_dark_theme_for("cybercore-gold-color-theme.json", "cybercore-gold-dark-color-theme.json");
