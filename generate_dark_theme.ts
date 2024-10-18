60c89e57ce4e44398d841805d85f3196f0beb582
cactoes <maartenhvanrooij@gmail.com>
2024-10-18 13:01:34 +0200

    added purple theme

--- generate_dark_theme.ts
+++ generate_dark_theme.ts
@@ -66,3 +66,4 @@ function generate_dark_theme_for(in_file: string, out_file: string): void {
 
 generate_dark_theme_for("cybercore-color-theme.json", "cybercore-dark-color-theme.json");
 generate_dark_theme_for("cybercore-gold-color-theme.json", "cybercore-gold-dark-color-theme.json");
+generate_dark_theme_for("cybercore-purple-color-theme.json", "cybercore-purple-dark-color-theme.json");
